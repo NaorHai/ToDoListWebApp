68255c63874eba803f0afe21714bd8a093d99867
NaorHai <Na1994or>
2017-12-14 17:38:19 +0200

    initDB jar

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.DriverManager;
+
+
 /**
  * Created by Papushe on 14/12/2017.
  */
 public class Main {
     public static void main(String[] args) {
-
+        String script = "../resources/DBinit.sql";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            new ScriptRunner(DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/ToDo&useSSL=false", "root", "root`"))
+                    .runScript(new BufferedReader(new FileReader(script)));
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
