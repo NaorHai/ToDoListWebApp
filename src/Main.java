ac80d86fb0b913a36f70488741f13a40177286f6
Papushe <papushe9@gmail.com>
2018-01-04 14:51:04 +0200

    log4j properties new place to src/

--- src/Main.java
+++ src/Main.java
@@ -37,53 +37,53 @@ public class Main {
                 "content of item"
         );
 
-        //CREATE user
-        logger.info("Creating a new user " + user.toString()
-                + " success: " + userDAO.saveOrUpdate(user));
-
-        //CREATE item
-        logger.info("Creating a new item " + item.toString()
-                + " success: " + iToDoListDAO.saveOrUpdate(item));
-
-        //READ user
-        User storedUser = userDAO.getUserById(user.getUserId());
-        logger.info("Getting stored user " + storedUser.toString()
-                + " result: " + storedUser);
-
-        //READ item
-        List<Item> userItems = iToDoListDAO.getItemsByUserId(user.getUserId());
-        logger.info("Getting user items " + userItems.get(0).toString()
-                + " result: " + userItems);
-
-        //UPDATE user
-        storedUser.setEmail("new@mail.com");
-        logger.info("Updating a user " + storedUser.toString()
-                + " success: " + userDAO.saveOrUpdate(storedUser));
-
-        //UPDATE item
-        userItems.get(0).setTitle("new title");
-        logger.info("Updating an item " + userItems.get(0).toString()
-                + " success: " + iToDoListDAO.saveOrUpdate(userItems.get(0)));
-
-        //DELETE item
-        logger.info("Deleting User's item: " + item.getItemId()
-                + " success: " + iToDoListDAO.deleteItem(item));
-
-        //DELETE user (also deleting all existed user items
-        logger.info("Deleting user: " + storedUser.getUserId()
-                + " success: " + userDAO.deleteUser(storedUser));
-
-
-        //DELETE all user items
-        User u = new User("user@mail.com", "first", "last");
-        Item userItem1 = new Item(u.getUserId(), "title", "content");
-        Item userItem2 = new Item(u.getUserId(), "title", "content");
-
-        userDAO.saveOrUpdate(u);
-        iToDoListDAO.saveOrUpdate(userItem1);
-        iToDoListDAO.saveOrUpdate(userItem2);
-
-        logger.info("User's all items deletion success: "
-                + iToDoListDAO.deleteAllItemsByUserId(u.getUserId()));
+//        //CREATE user
+//        logger.info("Creating a new user " + user.toString()
+//                + " success: " + userDAO.saveOrUpdate(user));
+//
+//        //CREATE item
+//        logger.info("Creating a new item " + item.toString()
+//                + " success: " + iToDoListDAO.saveOrUpdate(item));
+//
+//        //READ user
+//        User storedUser = userDAO.getUserById(user.getUserId());
+//        logger.info("Getting stored user " + storedUser.toString()
+//                + " result: " + storedUser);
+//
+//        //READ item
+//        List<Item> userItems = iToDoListDAO.getItemsByUserId(user.getUserId());
+//        logger.info("Getting user items " + userItems.get(0).toString()
+//                + " result: " + userItems);
+//
+//        //UPDATE user
+//        storedUser.setEmail("new@mail.com");
+//        logger.info("Updating a user " + storedUser.toString()
+//                + " success: " + userDAO.saveOrUpdate(storedUser));
+//
+//        //UPDATE item
+//        userItems.get(0).setTitle("new title");
+//        logger.info("Updating an item " + userItems.get(0).toString()
+//                + " success: " + iToDoListDAO.saveOrUpdate(userItems.get(0)));
+//
+//        //DELETE item
+//        logger.info("Deleting User's item: " + item.getItemId()
+//                + " success: " + iToDoListDAO.deleteItem(item));
+//
+//        //DELETE user (also deleting all existed user items
+//        logger.info("Deleting user: " + storedUser.getUserId()
+//                + " success: " + userDAO.deleteUser(storedUser));
+//
+//
+//        //DELETE all user items
+//        User u = new User("user@mail.com", "first", "last");
+//        Item userItem1 = new Item(u.getUserId(), "title", "content");
+//        Item userItem2 = new Item(u.getUserId(), "title", "content");
+//
+//        userDAO.saveOrUpdate(u);
+//        iToDoListDAO.saveOrUpdate(userItem1);
+//        iToDoListDAO.saveOrUpdate(userItem2);
+//
+//        logger.info("User's all items deletion success: "
+//                + iToDoListDAO.deleteAllItemsByUserId(u.getUserId()));
     }
 }
--- src/com/todolist/service/IToDoListService.java
+++ src/com/todolist/service/IToDoListService.java
@@ -1,9 +1,33 @@
 package com.todolist.service;
 
-
+import com.todolist.dao.HibernateToDoListDAO;
+import com.todolist.dao.IToDoListDAO;
+import com.todolist.exception.item.ItemException;
+import com.todolist.pojo.Item;
+import org.apache.log4j.Logger;
 /**
- * Created by Haimov on 14/12/2017.
+ * Created by Papushe on 04/01/2018.
  */
-public interface IToDoListService {
+public class IToDoListService {
+    private final static Logger logger = Logger.getLogger(IToDoListService.class);
+    private IToDoListDAO iToDoListDAO = HibernateToDoListDAO.getInstance();
 
+    public IToDoListService(){}
+
+    /**
+     * creates a new item
+     * returns true in success or false in failure
+     */
+    public boolean createItem(String userId, String title, String content){
+        Item item = new Item(userId,title,content);
+        try {
+            iToDoListDAO.saveOrUpdate(item);
+            logger.info("created a new item successfully: " + item.toString());
+            return true;
+        } catch (ItemException e) {
+            e.printStackTrace();
+            logger.info("failed to create a new item: " + item.toString());
+            return false;
+        }
+    }
 }
